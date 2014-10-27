1040d04bd653a2cb4bebdcbd1a9e0f0f09d8a370
Raysmond <jiankunlei@126.com>
2014-10-27 09:18:53 +0800

    U

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -20,6 +20,14 @@
  * of rows in the triangle.
  */
 
+//
+//  main.cpp
+//  triangle
+//
+//  Created by Raysmond on 27/10/14.
+//  Copyright (c) 2014 Raysmond. All rights reserved.
+//
+
 #include <iostream>
 #include <vector>
 
@@ -30,25 +38,38 @@ public:
     int minimumTotal(vector<vector<int> > &triangle) {
         vector<int> dp(triangle.size(), 0);
         for(int i=0;i<triangle.size();++i){
-          if(i==0) dp[0] = triangle[0][0];
-          else{
-            for(int j=0;j<=i;++j){
-              if(j==0) dp[j] += triangle[i][j];
-              else if(j==i) dp[j] += triangle[i][j-1];
-              else{
-                dp[j] = triangle[i][j] + min(dp[j-1],dp[j]);
-              }
+            if(i==0) dp[0] = triangle[0][0];
+            else{
+                int tmp = 0, tmpOld = 0;
+                for(int j=0;j<=i;++j){
+                    tmp = dp[j];
+                    if(j==0) dp[j] += triangle[i][j];
+                    else if(j==i) dp[j] = triangle[i][j] + tmpOld;
+                    else{
+                        dp[j] = triangle[i][j] + min(tmpOld,dp[j]);
+                    }
+                    tmpOld = tmp;
+                }
             }
-          }
         }
         int min = dp[0];
         for(int i=0;i<dp.size();++i)
-          if(dp[i]<min)
-            min = dp[i];
+            if(dp[i]<min)
+                min = dp[i];
         return min;
     }
 };
 
-int main(){
-  return 0;
+int main(int argc, const char * argv[])
+{
+
+    vector<vector<int>> input;
+    vector<int> v1{-1};
+    vector<int> v2{-2,-3};
+    input.push_back(v1);
+    input.push_back(v2);
+    Solution s;
+    cout<<s.minimumTotal(input)<<endl;
+    return 0;
 }
+
